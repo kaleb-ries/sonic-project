3723cf4f3a0e1b4773d56499da422cdf58dbe3a5
kaleb-ries <kalebries09@gmail.com>
2025-07-17 09:53:41 -0700

    Set up volume and speaking rate settings and functionality

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sonic-project
 A prototype for audio navigation and games
+
+# Features
+* A main menu with audio announcements for settings and help
+* A submenu for settings with volume and rate, with functionality to change the volume and rate
+* Up and down arrow moves through menus
+* Left and right arrows change the value of a setting
+* Enter opens a menu or selects a setting so that it can be modified
+
+# Issues
+* Since this project is client-side HTML only, sounds cannot immediately be played upon opening the HTML file, so the currently selected item will not be announced when the HTML loads
+* Visual elements have not been added, and there is no guide to navigating the audio interface
--- sonic.js
+++ sonic.js
@@ -1,18 +1,44 @@
+var config = {
+volume: 80,
+rate: 50
+};
 const menu = [
 {
 name: "settings",
+config: config,
 sub: [
 {
-name: "volume",
+name: function () {
+return "rate|volume/"+menu[0].config.rate;
+},
 enter: () => {
-seek = volume;
+seek = config.rate;
+},
+sub: [
+{
+name: "rate",
+row: (dir) => {
+horizontal(dir);
+config.rate = seek;
+playAudio('volume/'+seek);
+}
+}
+]
+},
+{
+name: function () {
+return "volume|volume/"+menu[0].config.volume;
+},
+enter: () => {
+seek = config.volume;
 },
 sub: [
 {
 name: "volume",
 row: (dir) => {
 horizontal(dir);
-volume = seek;
+config.volume = seek;
+playAudio('volume/'+seek);
 }
 }
 ]
@@ -26,17 +52,15 @@ name: "help"
 
 var item = 0;
 var seek = 0;
-var volume = 80;
 var level = menu;
 var root = [];
 function opening() {
-let sound = new Howl({
-	src: [level[item].name+'.mp3'],
-	autoplay: true
-});
+playAudio(level[item].name);
 }
 document.addEventListener('keyup', (key) => {
-if (key.key === "ArrowUp") {
+if (key.key === "Control") {
+Howler.stop();
+} else if (key.key === "ArrowUp") {
 shift("up");
 } else if (key.key === "ArrowDown") {
 shift("down");
@@ -50,26 +74,20 @@ level[item].row("right");
 }
 } else if (key.key === "Enter") {
 if (level[item].enter) {
-level[item].enter;
+level[item].enter();
 }
 if (level[item].sub) {
 root.push(level);
 level = level[item].sub;
 item = 0;
-let sound = new Howl({
-	src: [level[item].name+'.mp3'],
-	autoplay: true
-});
+playAudio(level[item].name);
 }
 } else if (key.key === "Escape") {
 if (root.length !== 0) {
 level = root[root.length - 1];
 root.pop();
 item = 0;
-let sound = new Howl({
-	src: [level[item].name+'.mp3'],
-	autoplay: true
-});
+playAudio(level[item].name);
 }
 }
 });
@@ -80,23 +98,17 @@ item -= 1;
 } else if (dir === "down") {
 item += 1;
 }
-if (!menu[item]) {
+if (!level[item]) {
 if (dir === "up") {
-item = menu.length - 1;
+item = level.length - 1;
 } else if (dir === "down") {
 item = 0;
 } else {
 console.log("Invalid string given - must be either 'up' or 'down'");
 }
 }
-let sound1 = new Howl({
-	src: [level[item].name+'.mp3'],
-	autoplay: true
-});
-let sound2 = new Howl({
-	src: ['click.mp3'],
-	autoplay: true
-});
+playAudio(level[item].name);
+playAudio('click');
 }
 function horizontal(dir) {
 if (dir === "right") {
@@ -111,8 +123,31 @@ seek = 100;
 } else if (seek < 0) {
 seek = 0;
 }
+
+}
+function playAudio(path, extra) {
+
+var list;
+if (typeof path === "function") {
+list = path().split("|");
+} else {
+list = path.split("|");
+}
+Howler.stop();
+setTimeout(function () {
 let sound = new Howl({
-	src: ['volume/'+seek+'.mp3'],
+	src: [list[0]+'.mp3'],
+	html5: true,
+	volume: config.volume*0.01,
+	rate: (config.rate*0.01)+0.5,
 	autoplay: true
 });
+sound.on('end', () => {
+list.shift();
+if (list[0]) {
+console.log(list[0]);
+playAudio(list[0]);
+}
+});
+}, 1);
 }
